d790daf488898fbc55888e623b124f8fb4e3489b
Faopaw <66549556+Faopaw@users.noreply.github.com>
2021-05-26 07:56:52 +0100

    Cleaned up and commented code
    Code is almost fully functional.

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-// const parsedJSON = {
-//         "type": "FeatureCollection",
-//         "name": "Leicestershire Locations",
-//         "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-//         "features": [
-//         { "type": "Feature", "properties": { "Name": "Old John Tower", "description": "Bradgate Park" }, "geometry": { "type": "Point", "coordinates": [ 52.6963498, -1.2237636] } },
-//         { "type": "Feature", "properties": { "Name": "Beacon Hill, Leicestershire", "description": "Beacon Hill Country Park" }, "geometry": { "type": "Point", "coordinates": [ 52.72846, -1.24772] } },
-//         { "type": "Feature", "properties": { "Name": "Croft Hill", "description": null }, "geometry": { "type": "Point", "coordinates": [ 52.5656001, -1.2488721] } },
-//         { "type": "Feature", "properties": { "Name": "Hamilton", "description": null }, "geometry": { "type": "Point", "coordinates": [ 52.662891, -1.074298] } },
-//         { "type": "Feature", "properties": { "Name": "Hamilton", "description": null }, "geometry": { "type": "Point", "coordinates": [ 52.662773180339599, -1.07245964568234] } },
-//         { "type": "Feature", "properties": { "Name": "Bardon Hill Quarry", "description": null }, "geometry": { "type": "Point", "coordinates": [ 52.7144707, -1.3206114] } },
-//         { "type": "Feature", "properties": { "Name": "Porsche Garage", "description": "Hamilton Porsche\/Aldi" }, "geometry": { "type": "Point", "coordinates": [ 52.657914, -1.0828292] } },
-//         { "type": "Feature", "properties": { "Name": "Evington Lane", "description": null }, "geometry": { "type": "Point", "coordinates": [ 52.6214907, -1.0912036] } },
-//         { "type": "Feature", "properties": { "Name": "Beal Street", "description": null }, "geometry": { "type": "Point", "coordinates": [ 52.636269601952499, -1.11793914530396] } }
-//         ]
-//         }
-
 const body = document.getElementById("body");
 const cardsContainer = document.querySelector("#cardsContainer");
 const myform = document.getElementById("form");
 const locationInput = document.getElementById("locationInput");
 
-
+// JSON file.
 const parsedJSON = {
   "type": "FeatureCollection",
   "name": "Leicestershire Locations",
@@ -44,7 +27,7 @@ const parsedJSON = {
   ]
   }
 
-
+// Makes the HTML elements appear once the page is loaded
 const loadData = function(parsedJSON){
     for(let i = 0; i < parsedJSON.features.length ; i++){
         
@@ -52,11 +35,6 @@ const loadData = function(parsedJSON){
         const divCard = document.createElement("div");
         divCard.classList.add("card");
         divCard.style.width = "100%";
-
-        // const img = document.createElement("img");
-        // img.classList.add("card-img-top");
-        // img.alt = "No Image Available"
-        // img.src = "images/No_image_3x4.svg.png";
 
         const divCardBody = document.createElement("div");
         divCardBody.classList.add("card-body");
@@ -109,60 +87,55 @@ const loadData = function(parsedJSON){
     }
 }
 
-// body.addEventListener("load", loadData(parsedJSON));
+  function initMap() {  
 
-let map;
-
-function initMap() {  
-
-loadData(parsedJSON);
+  loadData(parsedJSON);
 
 
-  // Map is centered around Leicester
-  const options = {
-    center: {lat: 52.6369, lng:-1.1398},
-    zoom: 10
-}
+    // Map is centered around Leicester
+    const options = {
+      center: {lat: 52.6369, lng:-1.1398},
+      zoom: 10
+  }
 
-// Creats a new map
-map = new google.maps.Map(document.getElementById('map'), options)
+  // Creats a new map
+  map = new google.maps.Map(document.getElementById('map'), options)
 
 
 
-//Make an array of markers
-const markersArray = parsedJSON.features;
+  //Make an array of markers
+  const markersArray = parsedJSON.features;
 
 
-// Loop through the array of markers and add them to the map
-for (let index = 0; index < markersArray.length; index++) {
-  addMarkers(markersArray[index])
-  
-}
-
-//function to add the marker to the map
-
-function addMarkers(properties){
-  const position = {lat:properties.geometry.coordinates[0], lng:properties.geometry.coordinates[1]}
-  const marker = new google.maps.Marker({
-    position : position,
-    map:map,
-    //icon:"https......" you can add your own custom marker here.
-  })
-
-  if (properties.properties.Name & properties.properties.description) {
-    const detailWindow = new google.maps.InfoWindow({
-    content: `${properties.properties.Name}, ${properties.properties.description}`
-    }) 
+  // Loop through the array of markers and add them to the map
+  for (let index = 0; index < markersArray.length; index++) {
+    addMarkers(markersArray[index])
     
-    marker.addListener("mouseover", () => { 
-    detailWindow.open(map,marker);
+  }
+
+  //function to add the marker to the map
+
+  function addMarkers(properties){
+    const position = {lat:properties.geometry.coordinates[0], lng:properties.geometry.coordinates[1]}
+    const marker = new google.maps.Marker({
+      position : position,
+      map:map,
+      //icon:"https......" you can add your own custom marker here.
     })
 
-}
+    if (properties.properties.Name & properties.properties.description) {
+      const detailWindow = new google.maps.InfoWindow({
+      content: `${properties.properties.Name}, ${properties.properties.description}`
+      }) 
+      
+      marker.addListener("mouseover", () => { 
+      detailWindow.open(map,marker);
+      })
+    }
 
 
 
-}
+  }
 
 
 }
@@ -178,6 +151,10 @@ const calculateDistanceMatrix = function() {
     unitSystem: google.maps.UnitSystem.IMPERIAL
   };
   // Call Distance Matrix service
+  console.log("origin => ",origin)
+  console.log("destinations => ",destinations)
+  console.log("matrixOptions.origins => ",matrixOptions.origins)
+  console.log("matrixOptions.destinations => ",matrixOptions.destinations)
   service.getDistanceMatrix(matrixOptions, callback);
 
   // Callback function used to process Distance Matrix response
@@ -212,12 +189,9 @@ const fetchOriginandDestinations = function(){
   const origin = (locationInput.value);
   const regex = ",";
   const comma = origin.search(regex);
-  console.log(comma);
   const lat = Number(origin.slice(0,comma -1));
-  console.log(origin)
   const lng = Number(origin.slice(comma + 1,origin.length - 1));
   const origins = {lat:lat,lng:lng}
-  console.log(origins)
 
   const destinations = []
 
@@ -226,7 +200,5 @@ const fetchOriginandDestinations = function(){
     const lng = parsedJSON.features[i].geometry.coordinates[1];
     destinations.push({lat:lat,lng:lng})
   }
-
-  console.log(destinations);
   return origins, destinations;
 }
